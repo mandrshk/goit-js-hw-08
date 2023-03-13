e751d81921f76cabde690e03ebfed886ff7f6ed9
Marina Andrushko <97413786+mandrshk@users.noreply.github.com>
2023-03-13 19:07:30 +0200

    task 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
-console.log('hi');
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createImageMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createImageMarkup (galleryItems)  {
+    return galleryItems.map(({preview, original, description}) => {
+            return `
+            <a class="gallery__item" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+            `
+    }).join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+  });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,30 @@
 
-console.log('hi');
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+
+const player = new  Player(iframe, {
+    loop: true,
+    fullscreen: true,
+    quality: '1080p',
+  });
+
+  const getCurrentTime = function (currentTime) {
+    const time = currentTime.seconds;
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(time));
+  }
+
+  player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+  player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0 );
+  
+  player
+  .setColor('#aabbaa')
+  .then(function (color) {
+    console.log('The new color value: #aabbaa');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
